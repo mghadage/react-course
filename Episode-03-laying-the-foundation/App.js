9ff41b2c321b00156b49bea5716bcc5699b7b077
mghadage <maheshghadage41@gmail.com>
2023-10-31 16:20:59 +0530

    episode 3 part 2 is done

--- Episode-03-laying-the-foundation/App.js
+++ Episode-03-laying-the-foundation/App.js
@@ -1,17 +1,8 @@
 import React  from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement('div',{id:"parent"},[
-                React.createElement('div',{id:"child1"},[
-                                        React.createElement('h1',{},'This is React Course 🚀🚩'),
-                                        React.createElement('h2',{},'by akshay saini')
-                                    ]),
-                React.createElement('div',{id:"child2"},[
-                                        React.createElement('h1',{},'Episode No. 03'),
-                                        React.createElement('h2',{},'Igniting our app')
-                                    ])
-            ]);
+const heading = React.createElement("h1",{id:"heading"},"Laying the foundation 🚀");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(heading);
