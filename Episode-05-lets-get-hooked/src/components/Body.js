f026664bda9708feb142bf94d9ca30b2aeabbcef
mghadage <maheshghadage41@gmail.com>
2023-11-06 00:50:05 +0530

    useState hookes learned and created a filter of top rated product

--- Episode-05-lets-get-hooked/src/components/Body.js
+++ Episode-05-lets-get-hooked/src/components/Body.js
@@ -1,16 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import restList from "../utils/mockData";
+import { useState } from "react";
 
 // not use key <<<<< use index as key <<<<<<<<<<<<<<<<<< use unique id as key
 const Body = () => {
+
+    const [listOfRestaurant,steListOfRestaurant] = useState(restList);
+
+    const filterData = () => {
+        const filteredData = listOfRestaurant.filter((res) => res.avgRating > 4);
+        steListOfRestaurant(filteredData);
+    }
+
     return (
         <div className="body">
-           <div className="search-container">
-                Search
+           <div className="filter-container">
+                <button className="filter-btn" onClick={filterData}>Top Rated Restaurant</button>
            </div>
            <div className="restaurant-comtainer"> 
             {
-                restList.map((restaurant) => (<RestaurantCard key={restaurant.id} restData={restaurant}  />))
+                listOfRestaurant.map((restaurant) => (<RestaurantCard key={restaurant.id} restData={restaurant}  />))
             }
            </div>
         </div>
--- Episode-05-lets-get-hooked/src/utils/mockData.js
+++ Episode-05-lets-get-hooked/src/utils/mockData.js
@@ -1,8 +1,9 @@
 const restList = [
     {id:"1",resName:"Burger King",courisal:"Burgers,American, Kandiwali West",avgRating:"4.5",costForTwo:"40000",deliveryTime:"30"},
-    {id:"2",resName:"KFC",courisal:"Burgers, Ulwe, Navi Mumbai",avgRating:"4.0",costForTwo:"20000",deliveryTime:"10"},
+    {id:"2",resName:"KFC",courisal:"Burgers, Ulwe, Navi Mumbai",avgRating:"3.9",costForTwo:"20000",deliveryTime:"10"},
     {id:"3",resName:"Sardarjis Kichan",courisal:"Biryani, Sector 17, Ulwe",avgRating:"3.8",costForTwo:"30000",deliveryTime:"20"},
     {id:"4",resName:"Roti Boti",courisal:"Roti Boti, Sector 16, Ulwe",avgRating:"4.7",costForTwo:"50000",deliveryTime:"15"},
+    {id:"5",resName:"Test",courisal:"Test, Sector 17, Ulwe",avgRating:"4.2",costForTwo:"50000",deliveryTime:"15"},
 ];
 
 export default restList;
