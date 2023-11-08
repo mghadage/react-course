69c18d31ce07fadde805d113ecf160ed97da4956
mghadage <maheshghadage41@gmail.com>
2023-11-08 20:12:02 +0530

    children route created and used with outlet component which is provided by react-router-dom

--- Episode-07-finding-the-path/src/App.js
+++ Episode-07-finding-the-path/src/App.js
@@ -1,6 +1,6 @@
 import React  from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
@@ -12,7 +12,7 @@ const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            <Outlet />
         </div>
     );
 }
@@ -21,16 +21,23 @@ const appRouter = createBrowserRouter([
                 {
                     path: "/",
                     element: <AppLayout />,
+                    children:[
+                        {
+                            path: "/",
+                            element: <Body />
+                        },
+                        {
+                            path: "/about",
+                            element: <About />
+                        },
+                        {
+                            path: "/contact",
+                            element: <Contact />
+                        },
+                    ],
                     errorElement: <Error />
                 },
-                {
-                    path: "/about",
-                    element: <About />
-                },
-                {
-                    path: "/contact",
-                    element: <Contact />
-                },
+                
             ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
